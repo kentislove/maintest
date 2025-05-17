5623dbc517c7124b45fff717c5df2abf57eb9cdd
kentislove <newlife691027@gmail.com>
2025-05-17 17:26:25 +0800

    Update maintest.py

--- maintest.py
+++ maintest.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 
 with gr.Blocks() as demo:
@@ -7,5 +8,6 @@ with gr.Blocks() as demo:
         gr.Textbox(label="你的問題")
         gr.Button("送出")
 
-demo.launch()
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))  # Render 會給你一個 PORT 環境變數
+    demo.launch(server_name="0.0.0.0", server_port=port)
